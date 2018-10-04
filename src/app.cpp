f63e8502435c99f9bfd3d7ef9dee36f2ad9c0e33
Derek Peterson <derekepterson.dpp@gmail.com>
2018-10-04 01:33:21 -0400

    test window

---
+++ src/app.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+    //Initialize glfw
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create window object
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // set size of opengl viewport to size of window
+    glViewport(0, 0, 800, 600);
+
+    // set callback to resize viewport to window size if the window size is changed
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Keep going until window should close
+    while (!glfwWindowShouldClose(window))
+    {
+        // swap buffers
+        glfwSwapBuffers(window);
+        // check if any events are triggered
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
