93e43803c90dc8e7c83ce19aed03e81fd239f139
Derek Peterson <derekpeterson.dpp@gmail.com>
2018-10-10 19:55:03 -0400

    rectangle using ebo

--- src/app.cpp
+++ src/app.cpp
@@ -115,19 +115,35 @@ int main()
     glDeleteShader(fragmentShader);
     
     // Triangle vertice data
-    float vertices[] = {
-        -0.5, -0.5, 0.0,
-        0.5, -0.5, 0.0,
-        0.0, 0.5, 0.0
-    };
+    //float vertices[] = {
+    //    -0.5, -0.5, 0.0,
+    //    0.5, -0.5, 0.0,
+    //    0.0, 0.5, 0.0
+    //};
+
+	// Rectange vertex/index data
+	float vertices[] = {
+	     0.5f,  0.5f, 0.0f,  // top right
+	     0.5f, -0.5f, 0.0f,  // bottom right
+	    -0.5f, -0.5f, 0.0f,  // bottom left
+	    -0.5f,  0.5f, 0.0f   // top left 
+	};
+	unsigned int indices[] = {  // note that we start from 0!
+	    0, 1, 3,   // first triangle
+	    1, 2, 3    // second triangle
+	};  
 
     // Create vertex buffer and vertex array objects
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	
 
     // Tell opengl how to interpret the vertice data
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
@@ -135,6 +151,9 @@ int main()
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+
+    // wireframe mode
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // Keep going until window should close
     while (!glfwWindowShouldClose(window))
@@ -149,7 +168,7 @@ int main()
         // swap buffers
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
         // check if any events are triggered
         glfwPollEvents();
