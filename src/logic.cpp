ecf17799d490d48391ad1577ed123cddebfa7f15
Derek Peterson <derekpeterson.dpp@gmail.com>
2019-01-08 10:43:30 -0500

    more progress on game logic

--- src/logic.cpp
+++ src/logic.cpp
@@ -1,14 +1,13 @@
 #include "logic.h"
 
 #include <fstream>
+#include <algorithm>
 
 using namespace logic;
 using namespace std;
 
 
 // TODO dynamically load deck from somewhere
-vector<logic::Card> SAMPLE_DECK;
-
 void GameState::writeStateToFile(string filename)
 {
     ofstream ofs(filename);
@@ -16,23 +15,60 @@ void GameState::writeStateToFile(string filename)
     oarchive(*this);
 }
 
+/* Create a n x n grid of systems 
+ * Initialize adjacency lists
+ */
+list<logic::System> createSystems(int n)
+{
+    vector<logic::System> systems;
+    systems.resize(n * n);
+
+    int dirs[][2] = {
+        {1, 0},
+        {-1, 0},
+        {0, 1},
+        {0, -1},
+        {1, -1},
+        {-1, 1}
+    };
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (auto dir : dirs) {
+                int adjacentPos[2] = {i + dir[0], j + dir[1]};
+                if (adjacentPos[0] < 0 or adjacentPos[0] >= n or
+                        adjacentPos[1] < 0 or adjacentPos[0] > n) {
+                    continue;
+                } else {
+                    systems[n * i + j].adjacent.push_back(
+                            systems[n * adjacentPos[0] + adjacentPos[1]].id);
+                }
+            }
+        }
+    }
+
+    return list<System> {begin(systems), end(systems)};
+}
+
 void GameState::startGame()
 {
     LOG_INFO << "Setting up game";
 
-    // TODO load decks from somewhere
-    LOG_INFO << "Loading sample deck";
-    for (int i = 0; i < 40; i++) {
-        SAMPLE_DECK.push_back({.name="Card", .cost={{RESOURCE_AI, 1}}});
-    }
+    LOG_INFO << "Initilializing systems";
+    systems = createSystems(4);
 
     LOG_INFO << "Creating player objects";
     for (int i = 0; i < 2; i++) {
         // Create player objects
+
+        // TODO load deck dynamically
+        list<logic::Card> deck;
+        for (int i = 0; i < 40; i++) {
+            deck.push_back({.name="Card " + to_string(i), .cost={{RESOURCE_AI, 1}}});
+        }
+
         Player p = {
             .name = "Player" + to_string(i),
-            .deck = SAMPLE_DECK,
-            .flagship = nullptr,
+            .deck = deck,
             .resources = {{RESOURCE_WARP_BEACONS, {.amount = 0, .max=1, .perTurn=1}}},
         };
         p.draw(7);
@@ -51,13 +87,221 @@ void GameState::startGame()
             .controller = i
         };
         ships.push_back(sampleFlagship);
+        p.flagshipId = sampleFlagship.id;
     }
 
     turnInfo = {
-        .whoseTurn = 0,
-        .activePlayer = 0,
-        .phase = PHASE_UPKEEP,
+        .whoseTurn = players.front().id,
+        .activePlayer = players.front().id,
+        .phase = {PHASE_MAIN},
     };
+};
+
+template <typename T>
+T* getById(int id, list<T> &vec)
+{
+    auto it = find_if(vec.begin(), vec.end(), [id] (T& t) {return t.id == id;});
+    if (it == vec.end()) {
+        return nullptr;
+    } else {
+        return &(*it);
+    }
+}
+
+Player* GameState::getPlayerById(int id) 
+{
+    return getById<Player>(id, players);
+}
+
+Ship* GameState::getShipById(int id) 
+{
+    return getById<Ship>(id, ships);
+}
+
+System* GameState::getSystemById(int id) 
+{
+    return getById<System>(id, systems);
+}
+
+Card* GameState::getCardById(int id) 
+{
+    list<list<Card>*> cardLists;
+    for (auto& p : players) {
+        cardLists.push_back(&p.deck);
+        cardLists.push_back(&p.hand);
+        cardLists.push_back(&p.discard);
+    }
+    
+    Card* card = nullptr;
+    for (auto l : cardLists) {
+        card = getById(id, *l);
+    }
+    return card;
+}
+
+vector<Action> GameState::getValidCardActions()
+{
+    // TODO more complicated logic
+    vector<Action> actions;
+
+    if (turnInfo.phase.back() == PHASE_MAIN 
+            or turnInfo.phase.back() == PHASE_RESOLVE_STACK) {
+        auto player = getPlayerById(turnInfo.whoseTurn);
+        for (auto& card : player->hand) {
+            Action action = {
+                .type = ACTION_PLAY_CARD,
+                .playerId = turnInfo.whoseTurn,
+                .id = card.id,
+                .description = card.name,
+            };
+            actions.push_back(action);
+        }
+    }
+
+    return actions;
+}
+
+vector<Action> GameState::getValidBeaconActions()
+{
+    // TODO more complicated logic
+    vector<Action> actions;
+    return actions;
+}
+
+vector<Action> GameState::getPossibleActions()
+{
+    vector<Action> actions;
+
+    auto cardActions = getValidCardActions();
+    auto moveActions = getValidBeaconActions();
+
+    Action pass = 
+    {
+        .type = ACTION_NONE,
+        .playerId = turnInfo.activePlayer,
+    };
+
+    Action endTurn =
+    {
+        .type = ACTION_END_TURN,
+        .playerId = turnInfo.activePlayer,
+    };
+
+    actions.push_back(pass);
+    if (turnInfo.phase.back() == PHASE_MAIN and turnInfo.activePlayer == turnInfo.whoseTurn) {
+        actions.push_back(endTurn);
+    }
+    actions.insert(actions.end(), cardActions.begin(), cardActions.end());
+    actions.insert(actions.end(), moveActions.begin(), moveActions.end());
+
+    return actions;
+}
+
+
+void GameState::performAction(Action action)
+{
+    switch (turnInfo.phase.back()) {
+        case PHASE_UPKEEP:
+            // TODO handle fast actions
+            LOG_INFO << "End upkeep, start main phase";
+            turnInfo.phase[0] = PHASE_MAIN;
+            break;
+        case PHASE_MAIN:
+            switch (action.type) {
+                case ACTION_PLAY_CARD:
+                    playCard(action.id, turnInfo.activePlayer); 
+                    // May change phase to a target select phase
+                    break;
+                case ACTION_PLACE_BEACON:
+                    placeBeacon(action.id, turnInfo.activePlayer);
+                    break;
+                case ACTION_NONE: 
+                case ACTION_END_TURN:
+                    turnInfo.phase[0] = PHASE_END;
+                    break;
+                case ACTION_SELECT_SHIPS:
+                case ACTION_SELECT_SYSTEM:
+                    LOG_ERROR << "Cannot select ships / system from main phase";
+                    break;
+                }
+        case PHASE_MOVE:
+            break; // TODO
+        case PHASE_END:
+            // TODO handle other player issues
+            endTurn();
+            break;
+        case PHASE_RESOLVE_STACK:
+            switch(action.type) {
+                case ACTION_PLAY_CARD:
+                    playCard(action.id, turnInfo.activePlayer);
+                    break;
+                case ACTION_NONE:
+                    resolveStackTop();
+                    break;
+                default:
+                    LOG_ERROR << "Invalid action type when resolving stack";
+            }
+            break;
+        case PHASE_SELECT_CARD_TARGETS:
+            break;
+        case PHASE_SELECT_BEACON_TARGETS:
+            break;
+    }
+}
+
+void GameState::playCard(int cardId, int playerId)
+{
+    auto player = getPlayerById(playerId);
+    auto it = find_if(player->hand.begin(), player->hand.end(), 
+            [cardId] (Card& c) {return c.id == cardId;});
+    it->playedBy = playerId;
+    stack.push_back(*it);
+    player->hand.erase(it);
+
+    if (turnInfo.phase.back() != PHASE_RESOLVE_STACK) {
+        turnInfo.phase.push_back(PHASE_RESOLVE_STACK);
+    }
+}
+
+void GameState::resolveStackTop()
+{
+    auto card = stack.back();
+    stack.pop_back();
+    card.resolve(*this);
+    auto player = getPlayerById(card.playedBy);
+    player->discard.push_back(card);
+    if (stack.size() == 0) {
+        turnInfo.phase.pop_back();
+    }
+}
+
+void GameState::placeBeacon(int systemId, int ownerId)
+{
+    WarpBeacon beacon = {
+        .ownerId = ownerId,
+        .systemId = systemId,
+    };
+    beacons.push_back(beacon);
+    turnInfo.phase[0] = PHASE_MOVE;
+    turnInfo.phase.push_back(PHASE_SELECT_CARD_TARGETS);
+}
+
+void GameState::endTurn()
+{
+    // Advance the turn to the next player
+    auto it = find_if(players.begin(), players.end(), 
+            [this] (Player& p) {return p.id == turnInfo.whoseTurn;});
+    LOG_INFO << "Turn ending for playerid " << it->id;
+    it++;
+    if (it == players.end()) {
+        it = players.begin();
+    }
+    auto nextPlayerId = it->id;
+    turnInfo.whoseTurn = nextPlayerId;
+    turnInfo.activePlayer = nextPlayerId;
+
+    LOG_INFO << "It is now player id " << nextPlayerId << "'s turn" << endl;
+    turnInfo.phase[0] = PHASE_UPKEEP;
 };
 
 void Player::draw(int n)
@@ -65,20 +309,109 @@ void Player::draw(int n)
     for (int i = 0; i < n; i++) {
         if (not deck.size()) {
             // TODO handle drawing from empty deck
+            // probably instant loss
             break;
         };
-        LOG_INFO << "Player " << name << " drew a card";
         auto card = deck.back();
         deck.pop_back();
         hand.push_back(card);
+        LOG_INFO << "Player " << name << " drew a card: " << card.name;
     }
 };
 
+ostream & logic::operator<< (ostream &out, const Action &c)
+{
+    out << "(Action: ";
+    switch (c.type) {
+        case ACTION_NONE:
+            out << "none"; break;
+        case ACTION_PLACE_BEACON:
+            out << "place beacon"; break;
+        case ACTION_PLAY_CARD:
+            out << "play card"; 
+            break;
+        case ACTION_END_TURN:
+            out << "end turn"; break;
+        case ACTION_SELECT_SHIPS:
+            out << "select ships"; break;
+        case ACTION_SELECT_SYSTEM:
+            out << "select system"; break;
+    }
+    if (c.description.size()) {
+        out << " " << c.description;
+    }
+    out << ")";
+
+    return out;
+}
+
+ostream & logic::operator<< (ostream &out, const TurnPhases &c)
+{
+    switch (c) {
+        case PHASE_UPKEEP:
+            out << "upkeep"; break;
+        case PHASE_MAIN:
+            out << "main"; break;
+        case PHASE_MOVE:
+            out << "move"; break;
+        case PHASE_END:
+            out << "end"; break;
+        case PHASE_SELECT_CARD_TARGETS:
+            out << "select card targets"; break;
+        case PHASE_SELECT_BEACON_TARGETS:
+            out << "select beacon targets"; break;
+        case PHASE_RESOLVE_STACK:
+            out << "resolve stack"; break;
+    }
+    return out;
+}
+
+ostream & logic::operator<< (ostream &out, const GameState &c)
+{
+    out << "GameState:\n";
+    out << "\tPlayer id of current turn: " << c.turnInfo.whoseTurn << endl;
+    out << "\tCurrent phase: " << c.turnInfo.phase << endl;
+    out << "\tPlayers:" << endl;
+    for (auto& p : c.players) {
+        cout << "\t\t" << p << endl;
+    }
+    out << "\tShips:" << endl;
+    for (auto& s : c.ships) {
+        cout << "\t\t" << s << endl;
+    }
+    out << "\tWarp Beacons:" << endl;
+    for (auto& b : c.beacons) {
+        cout << "\t\t" << b << endl;
+    }
+    out << "\tStack:" << endl;
+    for (auto& card : c.stack) {
+        out << "\t\t" << card << endl;
+    }
+    return out;
+}
+
+
+
 int main()
 {
-    plog::init(plog::verbose, "./logic_test.log");
+    static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
+    plog::init(plog::verbose, &consoleAppender);
 
     GameState state;
     state.startGame();
+    cerr << state;
     state.writeStateToFile("testState.json");
+    cerr << state;
+
+    while (1) {
+        auto actions = state.getPossibleActions();
+        int i = 0;
+        for (auto a : actions) {
+            cout << i++ << ": " << a << endl;
+        }
+        int selection;
+        cin >> selection;
+        state.performAction(actions[selection]);
+        cerr << state;
+    }
 }
--- src/logic.h
+++ src/logic.h
@@ -2,21 +2,27 @@
 #define LOGIC_H
 
 #include <vector>
+#include <list>
 #include <string>
 #include <map>
 #include <memory>
+#include <functional>
 
 #include <cereal/archives/json.hpp>
 #include <cereal/types/map.hpp>
 #include <cereal/types/vector.hpp>
+#include <cereal/types/list.hpp>
 #include <cereal/types/string.hpp>
 #include <cereal/types/complex.hpp>
 #include <cereal/types/memory.hpp>
 
 #include <prettyprint.hpp>
 #include <plog/Log.h>
+#include <plog/Appenders/ColorConsoleAppender.h>
 
 #include <stdarg.h>
+
+#include <backward.hpp>
 
 #define SERIALIZE(...) \
 template<class Archive> \
@@ -39,6 +45,11 @@ namespace logic {
 
     using namespace std;
 
+    class GameState;
+
+    /* Used to assign a unique id number to each game object when it is 
+     * created
+     * */
     struct GameObject {
         GameObject() {
             static int curId = 1;
@@ -47,39 +58,80 @@ namespace logic {
         int id;
     };
 
+    /* System object, representing a valid location for ship, structures
+     * etc to be placed */
     struct System : public GameObject
     {
         SERIALIZE(id);
+        vector<int> adjacent;
     };
 
+    /* Ship object, representing a unit that can move and attack other ship
+     * units */
     struct Ship : public GameObject
     {
-        string type;
-        int attack;
-        int shield;
-        int armour;
-        int movement;
+        string type;     // TODO this will be a key into some table
+        int attack;      // damage per round
+        int shield;      // regenerating hp
+        int armour;      // static hp
+        int movement;    // Number of systems can move in a single turn
 
-        int owner;
-        int controller;
+        int owner;       // playerId of owner
+        int controller;  // playerId of controller
 
-        SERIALIZE(type, attack, shield, armour, movement, controller);
+        int curSystemId; // Current location
+
+        friend ostream & operator << (ostream &out, const Ship &c)
+        {
+            out << "(Ship: " << c.type << " in system " << c.curSystemId << ")";
+            return out;
+        }
+
+        SERIALIZE(id, type, attack, shield, armour, movement, controller, 
+                curSystemId);
     };
 
+    struct WarpBeacon : public GameObject
+    {
+        int ownerId;    // Player who owns beacon
+        int systemId;   // System id where the beacon is currently placed
+
+        friend ostream & operator << (ostream &out, const WarpBeacon &c)
+        {
+            out << "(WarpBeacon: in system" << c.systemId << ")";
+            return out;
+        }
+        SERIALIZE(id, ownerId, systemId);
+    };
+
+    /* A resource amount, that could be a per turn changing amount
+     */
     struct Resource
     {
-        int amount;
-        int max;
-        int perTurn;
+        int amount;     // Current amount of resource present
+        int max;        // Current maximum resource capacity
+        int perTurn;    // Resource generation per turn
         SERIALIZE(amount, max, perTurn);
     };
 
+    // A type used to keep track of all resource types
     typedef map<ResourceType, int> ResourceAmount;
+
+    void DEFAULT_CARD_RESOLVE(GameState& state) {
+        LOG_ERROR << "Resolving a card with a default resolve function";
+    }
     
     struct Card : public GameObject
     {
         string name;
         ResourceAmount cost;
+        int playedBy = 0;
+        function<void(GameState&)> resolve = DEFAULT_CARD_RESOLVE;
+        function<vector<int>(GameState&)> getValidTargets;
+        friend ostream & operator << (ostream &out, const Card &c) {
+            out << "(Card: " << c.name << " id " << c.id << ")";
+            return out;
+        }
         SERIALIZE(name, cost);
     };
 
@@ -87,11 +139,17 @@ namespace logic {
     {
         string name;
         map<ResourceType, Resource> resources;
-        vector<Card> deck;
-        vector<Card> hand;
-        vector<Card> discard;
-        shared_ptr<Ship> flagship;
-        SERIALIZE(name, resources, deck, hand, discard, flagship);
+        list<Card> deck;
+        list<Card> hand;
+        list<Card> discard;
+        int flagshipId;
+        SERIALIZE(name, resources, deck, hand, discard, flagshipId);
+
+        friend ostream & operator << (ostream &out, const Player &c)
+        {
+            out << "(Player: " << c.name << " id " << c.id << ")";
+            return out;
+        }
 
         void draw(int n=1);
     };
@@ -101,29 +159,78 @@ namespace logic {
         PHASE_MAIN,
         PHASE_MOVE,
         PHASE_END,
+        PHASE_SELECT_CARD_TARGETS,
+        PHASE_SELECT_BEACON_TARGETS,
+        PHASE_RESOLVE_STACK,
     };
+
+    ostream & operator<< (ostream &out, const TurnPhases &c);
 
     struct TurnInfo
     {
         int whoseTurn;
         int activePlayer;
-        TurnPhases phase;
+        vector<TurnPhases> phase;
+        bool selectTargets;
         SERIALIZE(whoseTurn, activePlayer, phase);
+    };
+
+    enum ActionType {
+        ACTION_NONE,
+        ACTION_PLACE_BEACON,
+        ACTION_PLAY_CARD,
+        ACTION_END_TURN,
+        ACTION_SELECT_SHIPS,
+        ACTION_SELECT_SYSTEM,
+    };
+
+    struct Action
+    {
+        ActionType type;
+        int playerId;
+        int id;
+        vector<int> targets;
+        int nTargets;
+        string description;
+        friend ostream & operator << (ostream &out, const Action &c);
+        SERIALIZE(type, playerId, id, targets, nTargets, description);
     };
 
     class GameState
     {
         public:
             void startGame();
+            vector<Action> getPossibleActions();
+            void performAction(Action);
+
             void writeStateToFile(string filename);
             void print();
             SERIALIZE(turnInfo, players, ships, systems);
-        protected:
+
+            friend ostream & operator << (ostream &out, const GameState &c);
+
             TurnInfo turnInfo;
 
-            vector<Player> players;
-            vector<Ship> ships;
-            vector<System> systems;
+            Player* getPlayerById(int id);
+            Ship* getShipById(int id);
+            System* getSystemById(int id);
+            Card* getCardById(int id);
+            WarpBeacon* getBeaconById(int id);
+
+            void playCard(int cardId, int playerId);
+            void resolveStackTop();
+            void placeBeacon(int systemId, int ownerId);
+            void endTurn();
+
+            vector<Action> getValidCardActions();
+            vector<Action> getValidBeaconActions();
+
+            list<Player> players;
+            list<Ship> ships;
+            list<System> systems;
+            list<WarpBeacon> beacons;
+
+            list<Card> stack;
     };
 };
 
