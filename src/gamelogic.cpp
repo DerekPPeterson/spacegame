2570e213879fdabab1c553e38cad9a13f3996e64
Derek Peterson <derekpeterson.dpp@gmail.com>
2019-01-31 11:14:54 -0500

    reveal cards played by opponent

--- src/gamelogic.cpp
+++ src/gamelogic.cpp
@@ -225,6 +225,7 @@ void GraphicsObjectHandler::updateState(std::vector<logic::Change> changes)
                 {
                     auto cardId = get<int>(change.data);
                     auto card = dynamic_pointer_cast<Card>(getObject(cardId));
+                    card->setFaceUp(true);
                     hand->removeCard(card);
                     enemyHand->removeCard(card);
                     stack->addCard(card);
